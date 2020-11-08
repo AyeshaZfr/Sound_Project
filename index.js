286c319605536ea22ccc5e75ebc64fc4d0e9c086
Ayesha <ayeshamzfr@gmail.com>
2020-11-08 23:42:04 +0400

    1

--- index.js
+++ index.js
@@ -46,3 +46,47 @@ $(".location").click(function()
 
 // var name-file = 'images/' + name + '.jpeg';
 // $(".image-360").attr("src", "images/palms.jpeg") ;
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+
+/* --------------------------------------- */
+/* Ayesha's part begins */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+function openSubMenu1() {
+  document.getElementById("subMenu01").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
+function openSubMenu2() {
+  document.getElementById("subMenu02").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
+
+function openSubMenu3() {
+  document.getElementById("subMenu03").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
+
+function closeSubMenu() {
+  document.getElementById("subMenu01").style.width = "0";
+  document.getElementById("subMenu02").style.width = "0";
+  document.getElementById("subMenu03").style.width = "0";
+  document.getElementById("main").style.marginLeft = "250px";
+document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
+/* Ayesha's part ends */
+/* --------------------------------------- */
