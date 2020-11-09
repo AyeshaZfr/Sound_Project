089b70c1549a26497519d8d6dfc81208bf7801b2
Muhammad Anas <anas@Anas-MacBook-Pro.local>
2020-11-10 02:23:47 +0400

    a

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// $(document).ready(function()
+// {
+//   var a = new Audio();
+//   a.src = $(".audio-360").attr("src");
+//   a.play();
+// });
+
 // alert("hello");
 // // var location = $(".location");
 // // var overlay = $("span");
@@ -18,19 +25,94 @@
 // {
 //   alert("clicked");
 // });
+//
+// var a = new Audio();
+// a.src = $(".audio-360").attr("src");
+// a.play();
+//
+// $(".location").click(function() {
+//   name = $(this).attr("id");
+//   console.log(name);
+//   var name_img = 'images/' + name + '.jpeg';
+//   $(".image-36").attr("src", name_img);
+//   console.log($(".image-36").attr("src"));
+//   var name_audio = 'sounds/' + name + '.mp3';
+//   $(".audio-360").attr("src", name_audio);
+//   console.log($(".audio-360").attr("src"));
+//   //window.location = "location.html";
+// });
 
-$(".location").click(function()
-{
+var a = new Audio();
+$(".location").hover(function() {
   name = $(this).attr("id");
-  console.log(name);
-  var name_img = 'images/' + name + '.jpeg';
-  $(".image-36").attr("src", name_img);
-  console.log($(".image-36").attr("src"));
   var name_audio = 'sounds/' + name + '.mp3';
-  $(".audio-360").attr("src", name_audio);
+  a.src = name_audio;
+  a.play();
   console.log($(".audio-360").attr("src"));
-  //window.location = "location.html";
+}, function()
+{
+  a.pause();
 });
+
+
+var c = false;
+// $(".audio-360").attr("controls", true);
+$(".audio-360").attr("controls", false);
+
+$(".audio-hider").click(function()
+{
+  if (c == false)
+  {
+    $(".audio-360").attr("controls", true);
+    console.log("clicked");
+    console.log(c);
+    c = true;
+  }
+  else
+  {
+    $(".audio-360").attr("controls", false);
+    console.log("clicked");
+    console.log(c);
+
+    c=false;
+}
+});
+
+
+
+  // else
+  // {
+  //   $(".audio-360").attr("controls", false);
+  //   console.log("clicked");
+  //   console.log(c);
+  //
+  //   c=0;
+
+  // if ($(".audio-360").controls == "controls")
+  // {
+  //   $(".audio-360").attr("controls", false);
+  //   console.log("clicked");
+  // }
+  // console.log(($(".audio-360").attr("controls")));
+  // alert(($(".audio-360").attr("controls")))
+
+
+// $(".audio-shower").click(function() {
+//     $(".audio-360").attr("controls", false);
+// });
+
+
+// $(".audio-hider").hover(function()
+// {
+//   $(".audio-360").controls = true;
+//   console.log("clicked");
+//   console.log($(".audio-360"));
+// }, function()
+// {
+//   console.log("clicked");
+//   console.log($(".audio-360"));
+//   $(".audio-360").controls = true;
+// });
 
 // $(".location").hover(function()
 // {
@@ -69,16 +151,17 @@ function openSubMenu1() {
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 };
+
 function openSubMenu2() {
   document.getElementById("subMenu02").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 };
 
 function openSubMenu3() {
   document.getElementById("subMenu03").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 };
 
 function closeSubMenu() {
@@ -86,7 +169,7 @@ function closeSubMenu() {
   document.getElementById("subMenu02").style.width = "0";
   document.getElementById("subMenu03").style.width = "0";
   document.getElementById("main").style.marginLeft = "250px";
-document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 };
 /* Ayesha's part ends */
 /* --------------------------------------- */
